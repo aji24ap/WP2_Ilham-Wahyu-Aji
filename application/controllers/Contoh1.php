d9bf81e9ae3f97a350432ecbcf3001f58e09bc1e
aji24ap <19210697@bsi.ac.id>
2022-09-26 15:39:04 +0700

    menambahkan file pustaka-booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Ilham Wahyu Aji Saya tingga di daerah Serang Baru olah raga yang saya sukai adalah Bulutangkis";
+    }
+}
